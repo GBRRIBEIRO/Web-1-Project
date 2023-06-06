785ae9d66ebc0202c29b78870e222f91d503befc
LAB-ENG-PC15 <gabrieldexribeiro@hotmail.com>
2023-06-05 21:48:13 -0300

    Made first template

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var dataObj = JSON.parse(dataList);
 
 const server = http.createServer((request, response) => {
     
-    const cu = dataObj.map((element) => console.log(element));
 });
+
 server.listen(2700, '127.0.0.1', () => console.log("Server STARTED!"));
